d10f5c7a7504dda8c7048f9f3cb2492e405a4cc9
Leila Peltier <l_peltier@outlook.com>
2024-03-26 22:38:38 +0000

    changed "type of animal" description in pet component to "type" description

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -2,7 +2,7 @@ const Pet = ({pet}) => {
     return ( 
         <>
         <h3>{pet.name} (age {pet.age}) </h3>
-        <p>Type of Animal: {pet.type}</p>
+        <p>Type: {pet.type}</p>
         <p>Breed: {pet.breed}</p>
         
         </>
