6a98bce127852562bba696eab5e1f81d8ce0272a
Leila Peltier <l_peltier@outlook.com>
2024-03-26 22:15:32 +0000

    Able to load pet data using GET requests to localhost:8080/pets

--- src/containers/PetContainer.jsx
+++ src/containers/PetContainer.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import AddPetForm from "../components/AddPetForm";
 import PetList from "../components/PetList";
 
 const PetContainer = () => {
+    const [pets, setPets] = useState([]);
+
+    const fetchPets = async () =>{
+        const response = await fetch("http://localhost:8080/pets");
+        const petJson = await response.json();
+        setPets(petJson);
+    }
+
+    useEffect(() =>{
+        fetchPets();
+
+    },[])
+
+
     return ( 
         <>
         <header>
