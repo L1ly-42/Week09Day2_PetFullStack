368bcbc1cc31d4a680787f18c70008e49425d2bb
Leila Peltier <l_peltier@outlook.com>
2024-03-26 22:59:50 +0000

    Add pet form skeleton complete

--- src/components/AddPetForm.jsx
+++ src/components/AddPetForm.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
 const AddPetForm = () => {
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+    const [breed, setBreed] = useState("");
+    const [age, setAge] = useState(0);
     return ( 
         <>
-        <p>Add pet here :) </p>
+        <h3>Add pet:</h3>
+        <form>
+            <input 
+            type="text"
+            placeholder="Pet Name"
+            value={name}
+            onChange={(e)=> setName(e.target.value)} />
+
+            <input 
+            type="text"
+            placeholder="Type of Animal"
+            value={type}
+            onChange={(e)=> setType(e.target.value)} 
+            />
+
+            <input 
+            type="text"
+            placeholder="Breed"
+            value={breed}
+            onChange={(e)=> setBreed(e.target.value)} 
+             />
+
+             <input 
+             type="number"
+             min={0} 
+             max={600}
+             placeholder="Age"
+             value={age}
+            onChange={(e)=> setAge(e.target.value)} 
+             />
+
+             <input type="Submit" value="Submit"/>
+
+        </form>
+       
         </>
      );
 }
